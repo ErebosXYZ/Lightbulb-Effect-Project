3cfe61e5e9635b5062d4c24c054042a97f970dd3
ErebosXYZ <robertmaynou@gmail.com>
2025-03-26 21:28:03 +0100

    done + bonus

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+// Hem d'afegir un event on click que canviï la classe de bulb-off a bulb-on
+let bulb = document.querySelector("#bulb");
+
+document.querySelector("#bulb").addEventListener("mouseover", function (){
+    bulb.classList.toggle("bulb-on");
+});
+document.querySelector("#bulb").addEventListener("mouseout", function (){
+    bulb.classList.toggle("bulb-on");
+});
